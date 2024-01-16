a72780a1d924302e69c17962381f92f3536a69a7
Lee Mclafferty <lee.mclafferty@gmail.com>
2024-01-15 23:51:24 -0800

    Learning the flow of using open gl and the binding context

--- Src/Core/Engine.cpp
+++ Src/Core/Engine.cpp
@@ -9,16 +9,71 @@ Engine::Engine(GLFWwindow* glWin)
 
 void Engine::MainLoop()
 {
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
+		DrawCube();
 	}
+}
+
+void Engine::DrawCube()
+{
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Generate and bind VBO
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+	// Generate and bind EBO
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+
+	// Set vertex attribute pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind the VBO (optional, for cleanliness)
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Compile the vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for shader compile errors...
+
+	// Compile the fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Check for shader compile errors...
+
+	// Create shader program and link shaders to it
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors...
+
+	// Delete shaders; they're linked into the program now and no longer necessary
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Unbind VAO (optional, for cleanliness)
+	glBindVertexArray(0);
+
+	// Drawing
+	glUseProgram(shaderProgram); // Use the compiled shader program
+	glBindVertexArray(VAO); // Bind the vertex array object
+	glDrawElements(GL_LINES, sizeof(cubeIndices) / sizeof(cubeIndices[0]), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0); // Unbind VAO
+	glUseProgram(0); // Unbind shader program
+
 }
--- Src/Core/Engine.h
+++ Src/Core/Engine.h
@@ -12,5 +12,50 @@ public:
 private:
 	class GLFWwindow* window;
 
+	// Will be moved out of engine later.
+	void DrawCube();
+
+	float cubeVertices[24] = {
+	-0.5f, -0.5f, -0.5f, // Vertex 0
+	 0.5f, -0.5f, -0.5f, // Vertex 1
+	 0.5f,  0.5f, -0.5f, // Vertex 2
+	-0.5f,  0.5f, -0.5f, // Vertex 3
+	-0.5f, -0.5f,  0.5f, // Vertex 4
+	 0.5f, -0.5f,  0.5f, // Vertex 5
+	 0.5f,  0.5f,  0.5f, // Vertex 6
+	-0.5f,  0.5f,  0.5f  // Vertex 7
+	};
+
+	unsigned int cubeIndices[24] = {
+	0, 1, 1, 2, 2, 3, 3, 0, // Bottom Face
+	4, 5, 5, 6, 6, 7, 7, 4, // Top Face
+	0, 4, 1, 5, 2, 6, 3, 7  // Side Edges
+	};
+
+	const char* vertexShaderSource = R"glsl(
+		#version 330 core
+		layout (location = 0) in vec3 aPos; // the position variable has attribute position 0
+
+		uniform mat4 model;
+		uniform mat4 view;
+		uniform mat4 projection;
+
+		void main()
+		{
+			gl_Position = projection * view * model * vec4(aPos, 1.0); // Transforms vertex position to clip space
+		}
+		)glsl";
+
+	const char* fragmentShaderSource = R"glsl(
+		#version 330 core
+		out vec4 FragColor;
+
+		void main()
+		{
+			FragColor = vec4(1.0, 1.0, 1.0, 1.0); // Set the color to white
+		}
+		)glsl";
+
+
 };
 
